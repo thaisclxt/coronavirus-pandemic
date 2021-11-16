c8ba90b64cf0bb8ff623d9ffb1ef7297ae56ed1a
thaisclxt <thais.calixto@mail.uft.edu.br>
2021-11-16 11:53:32 -0300

    Refactor code

--- main.dart
+++ main.dart
@@ -1,13 +1,19 @@
 import 'dart:io';
 
-void getPartlyVaccinated(
-    List<double> fullyVaccinated, List<double> vaccinated) {
+void getPartlyVaccinated(fullyVaccinated) {
   var partlyVaccinated = <String>[];
+
+  fullyVaccinated.map((e) => double.parse(e)).toList();
+  final vaccinated = File('./people_vaccinated.txt')
+      .readAsLinesSync()
+      .map((e) => double.parse(e))
+      .toList();
 
   for (var i = 0; i < fullyVaccinated.length; i++) {
     partlyVaccinated
         .add((vaccinated[i] - fullyVaccinated[i]).toStringAsFixed(2));
   }
+
   File('partly_vaccinated.txt').openWrite().write(partlyVaccinated.join('\n'));
 }
 
@@ -16,15 +22,8 @@ void main(List<String> args) {
 
   final fullyVaccinated =
       File('./people_fully_vaccinated.txt').readAsLinesSync();
-  // .map((e) => double.parse(e))
-  // .toList();
 
-  // final vaccinated = File('./people_vaccinated.txt')
-  //     .readAsLinesSync()
-  //     .map((e) => double.parse(e))
-  //     .toList();
-
-  // //getPartlyVaccinated(fullyVaccinated, vaccinated);
+  // getPartlyVaccinated(fullyVaccinated);
 
   final partlyVaccinated = File('./partly_vaccinated.txt').readAsLinesSync();
 
