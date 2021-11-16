da54cae6631566384f5dc49f71b25405d9e0e70b
thaisclxt <thais.calixto@mail.uft.edu.br>
2021-11-16 13:16:59 -0300

    Normalize data

--- main.dart
+++ main.dart
@@ -17,6 +17,19 @@ void getPartlyVaccinated(fullyVaccinated) {
   File('partly_vaccinated.txt').openWrite().write(partlyVaccinated.join('\n'));
 }
 
+List<String> normalize(List<double> data) {
+  var normalizedData = <String>[];
+
+  final double min = data.reduce((value, next) => value < next ? value : next);
+  final double max = data.reduce((value, next) => value > next ? value : next);
+
+  for (var i = 0; i < data.length; i++) {
+    normalizedData.add(((data[i] - min) / (max - min)).toStringAsFixed(2));
+  }
+
+  return normalizedData;
+}
+
 void main(List<String> args) {
   final countries = File('./countries.txt').readAsLinesSync();
 
@@ -30,19 +43,31 @@ void main(List<String> args) {
   final cases = File('./cases.txt').readAsLinesSync();
   final deaths = File('./deaths.txt').readAsLinesSync();
 
+  final normalizedFullyVaccinated =
+      normalize(fullyVaccinated.map((e) => double.parse(e)).toList());
+
+  final normalizedPartlyVaccinated =
+      normalize(partlyVaccinated.map((e) => double.parse(e)).toList());
+
+  final normalizedCases =
+      normalize(cases.map((e) => double.parse(e.replaceAll(',', ''))).toList());
+
+  final normalizedDeaths = normalize(
+      deaths.map((e) => double.parse(e.replaceAll(',', ''))).toList());
+
   var attributes = [];
 
   for (var i = 0; i < countries.length; i++) {
     attributes.add(
       countries[i].trim() +
           ', ' +
-          fullyVaccinated[i].toString().trim() +
+          normalizedFullyVaccinated[i] +
           ', ' +
-          partlyVaccinated[i].trim() +
+          normalizedPartlyVaccinated[i] +
           ', ' +
-          cases[i].trim() +
+          normalizedCases[i] +
           ', ' +
-          deaths[i].trim(),
+          normalizedDeaths[i],
     );
   }
 
